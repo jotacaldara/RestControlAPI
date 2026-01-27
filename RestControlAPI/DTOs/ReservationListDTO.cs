d43e80673c5779469ac60ce094230dd7866225c1
I230067 <I230067@CTEI13>
2026-01-27 16:56:25 +0000

    27/01

---
+++ RestControlAPI/DTOs/ReservationListDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RestControlAPI.DTOs
+{
+    public class ReservationListDTO
+    {
+        public int Id { get; set; }
+        public string RestaurantName { get; set; } = string.Empty;
+        public DateTime ReservationDate { get; set; }
+        public int NumberOfPeople { get; set; }
+        public string Status { get; set; } = "Pending";
+    }
+}
